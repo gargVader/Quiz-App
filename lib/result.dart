ee54e930d0370edc2147be4c6e31f8c09bc8e0c3
gargVader <girishgargcool@gmail.com>
2021-08-08 15:52:58 +0530

    #53 done

--- lib/result.dart
+++ lib/result.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-
   int totalScore;
 
   Result(this.totalScore);
 
+  String get resultPhrase{
+    String resultText;
+    if(totalScore<=20){
+      resultText = 'Poor score';
+    }else{
+      resultText = 'Well done!';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Your score is "+totalScore.toString()));
+    return Center(
+        child: Text(
+      resultPhrase,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 30
+      ),
+    ));
   }
 }
